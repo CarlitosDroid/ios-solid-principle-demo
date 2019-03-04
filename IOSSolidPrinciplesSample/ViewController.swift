92bed82e480ca4ed540eb76a9126ccc2d1e7aaeb
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-03-03 19:46:07 -0500

    Initial Commit

---
+++ IOSSolidPrinciplesSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSSolidPrinciplesSample
+//
+//  Created by Carlos Leonardo Camilo Vargas Huamán on 3/3/19.
+//  Copyright © 2019 Carlos Leonardo Camilo Vargas Huamán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IOSSolidPrinciplesSampleTests/IOSSolidPrinciplesSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOSSolidPrinciplesSampleTests.swift
+//  IOSSolidPrinciplesSampleTests
+//
+//  Created by Carlos Leonardo Camilo Vargas Huamán on 3/3/19.
+//  Copyright © 2019 Carlos Leonardo Camilo Vargas Huamán. All rights reserved.
+//
+
+import XCTest
+@testable import IOSSolidPrinciplesSample
+
+class IOSSolidPrinciplesSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
